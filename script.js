3fca21eb4a9039ef57f916a6160ed1f6b28f3d4e
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-02 19:13:03 -0300

    Requuisitos 6 e 7

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const list = document.querySelector('#lista-tarefas');
 
-
+/*function clearTask() {
+    document.querySelector('#texo-tarefa').value='';
+}*/
 
 function newTask() {
     const task = document.querySelector('#texto-tarefa').value;
     const itemList = document.createElement('li');
     itemList.innerText = task;
     list.appendChild(itemList);
-    console.log(task)
+    document.getElementById('texto-tarefa').value='';
 }
 
 const bttn = document.querySelector('#criar-tarefa');
 bttn.addEventListener('click', newTask);
 
-console.log('qualq')
-    
