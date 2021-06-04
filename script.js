7b2849a7ed5f1a3458738dfcef03a3145b1006a5
guiiffonseca <guii.ffonseca22@gmail.com>
2021-06-04 14:26:01 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+//Requisitos 5 e 6
 const list = document.querySelector('#lista-tarefas');
-
+const bttn = document.querySelector('#criar-tarefa');
+bttn.addEventListener('click', newTask);
 function newTask() {
     const task = document.querySelector('#texto-tarefa').value;
     const itemList = document.createElement('li');
@@ -8,18 +10,24 @@ function newTask() {
     document.getElementById('texto-tarefa').value='';
 }
 
-const bttn = document.querySelector('#criar-tarefa');
-bttn.addEventListener('click', newTask);
-
-const listItem = document.querySelector('#lista-tarefas');
-listItem.addEventListener('click', backgroundItem);
-
+//Requisitos 7 e 8
+list.addEventListener('click', backgroundItem);
 function backgroundItem(event){
-    const item = document.querySelector('#lista-tarefas').children;
+   const item = document.querySelector('#lista-tarefas').children;
     for (let index = 0; index < item.length; index += 1){
         item[index].style.backgroundColor = '';
     }
     
     event.target.style.backgroundColor = 'rgb(128,128,128)';
-    
+}
+
+//Requisito 9
+list.addEventListener('dblclick', completedTask);
+
+function completedTask (event) {
+    if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed')
+    } else {
+    event.target.classList.add('completed')
+    }
 }
